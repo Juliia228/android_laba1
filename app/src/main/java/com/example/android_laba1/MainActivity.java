470fc38d82fd6876cbd67743812e9e7dffe8387f
Juliia228 <miss-komarova2017@yandex.ru>
2023-10-26 04:11:51 +0300

    Added code

--- app/src/main/java/com/example/android_laba1/MainActivity.java
+++ app/src/main/java/com/example/android_laba1/MainActivity.java
@@ -1,4 +1,32 @@
 package com.example.android_laba1;
 
-public class MainActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    TextView inputText;
+    EditText background;
+    Button btnNextPage;
+
+    public void onCreate (Bundle savedInstanceState) {
+        super .onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        inputText = (TextView) findViewById(R.id.input);
+        background = (EditText) findViewById(R.id.color);
+        btnNextPage = (Button) findViewById(R.id.btnNextPage);
+        btnNextPage.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        String color = background.getText().toString();
+        if (color.equals("red") || color.equals("green") || color.equals("blue")) {
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("background", color);
+            startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/com/example/android_laba1/SecondActivity.java
+++ app/src/main/java/com/example/android_laba1/SecondActivity.java
@@ -1,4 +1,34 @@
 package com.example.android_laba1;
 
-public class SecondActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends Activity implements View.OnClickListener {
+    Button btnBack;
+    public void onCreate (Bundle savedInstanceState) {
+        super .onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        String background = getIntent().getStringExtra("background").toString();
+        View secondView = findViewById(R.id.second);
+        switch (background) {
+            case "red":
+                secondView.setBackgroundColor(getApplicationContext().getColor(R.color.red));
+                break;
+            case "green":
+                secondView.setBackgroundColor(getApplicationContext().getColor(R.color.green));
+                break;
+            case "blue":
+                secondView.setBackgroundColor(getApplicationContext().getColor(R.color.blue));
+                break;
+            default:
+                secondView.setBackgroundColor(getApplicationContext().getColor(R.color.white));
+        }
+        btnBack = (Button) findViewById(R.id.btnGoBack);
+        btnBack.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        finish();
+    }
 }
